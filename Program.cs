6eb2d2874da8a277bf37e73bc6cde76fa67881f1
SeanSobey <sean.m.sobey@gmail.com>
2017-02-01 22:28:47 +0200

    EllysSubstringSorter

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-			var result = new EllysNumberGuessing().getNumber(new [] { 500000,600000,700000 }, new [] { 120013,220013,79987 });
+			//var result = new EllysNumberGuessing().getNumber(new [] { 500000,600000,700000 }, new [] { 120013,220013,79987 });
+			var result = new EllysSubstringSorter().getMin("TOPCODER", 4);
         }
     }
 }
