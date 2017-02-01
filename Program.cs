b9cea15806fb1af07a4c9032d07052a087a837be
SeanSobey <sean.m.sobey@gmail.com>
2017-02-01 23:47:15 +0200

    ATaleOfThreeCities

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApplication
         {
 			//var result = new EllysNumberGuessing().getNumber(new [] { 500000,600000,700000 }, new [] { 120013,220013,79987 });
 			//var result = new EllysSubstringSorter().getMin("TOPCODER", 4);
+			//var result1 = new ATaleOfThreeCities().connect(new []{0,0,0}, new []{0,1,2}, new []{2,3}, new []{1,1}, new []{1,5}, new []{3,28});
+			var result2 = new ATaleOfThreeCities().connect(new []{-2,-1,0,1,2}, new []{0,0,0,0,0}, new []{-2,-1,0,1,2}, new []{1,1,1,1,1}, new []{-2,-1,0,1,2}, new []{2,2,2,2,2});
         }
     }
 }
