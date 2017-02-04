1ae8edda6c0a7399d2166554b896423eee6c3ce6
Sean Sobey <sean.m.sobey@gmail.com>
2017-02-04 16:14:52 +0200

    Added WIP abacus

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace ConsoleApplication
+﻿namespace ConsoleApplication
 {
     public class Program
     {
@@ -10,8 +8,9 @@ namespace ConsoleApplication
 			//var result = new ABoardGame().whoWins(new string[] {".....A", "......", "..A...", "...B..", "......", "......"});
 			//var result = new EllysNumberGuessing().getNumber(new [] { 500000,600000,700000 }, new [] { 120013,220013,79987 });
 			//var result = new EllysSubstringSorter().getMin("TOPCODER", 4);
-			//var result1 = new ATaleOfThreeCities().connect(new []{0,0,0}, new []{0,1,2}, new []{2,3}, new []{1,1}, new []{1,5}, new []{3,28});
-			var result2 = new ATaleOfThreeCities().connect(new []{-2,-1,0,1,2}, new []{0,0,0,0,0}, new []{-2,-1,0,1,2}, new []{1,1,1,1,1}, new []{-2,-1,0,1,2}, new []{2,2,2,2,2});
+			//var result = new ATaleOfThreeCities().connect(new []{0,0,0}, new []{0,1,2}, new []{2,3}, new []{1,1}, new []{1,5}, new []{3,28});
+			//var result = new ATaleOfThreeCities().connect(new []{-2,-1,0,1,2}, new []{0,0,0,0,0}, new []{-2,-1,0,1,2}, new []{1,1,1,1,1}, new []{-2,-1,0,1,2}, new []{2,2,2,2,2});
+			var result = new Abacus().add(new [] {"ooo---oooooo", "---ooooooooo", "---ooooooooo", "---ooooooooo", "oo---ooooooo", "---ooooooooo"}, 5);
         }
     }
 }
