629994904418d9862117a5dd65e0cfdfdf7f1f4b
Sean Sobey <sean.m.sobey@gmail.com>
2017-02-05 12:00:33 +0200

    Added WIP AdjacentSwaps

---
+++ Problems/Medium/AdjacentSwaps.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class AdjacentSwaps
+{
+
+}
+public class AdjacentSwapsSolution : ISolution
+{
+	public bool Enabled { get; } = false;
+	public Action[] Theories { get; } = new Action[]
+	{
+		() => Console.WriteLine("" + "=" + ""), 
+	};
+}
